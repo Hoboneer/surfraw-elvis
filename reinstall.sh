f959debb44d849a9bedf69999da2acef240c72b5
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2019-11-22 14:49:51 +1300

    Add reinstall script

---
+++ reinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+PKG_MANAGER_CMD="${PKG_MANAGER_CMD:-pipsi}"
+UNINSTALL_CMD="$PKG_MANAGER_CMD uninstall"
+INSTALL_CMD="$PKG_MANAGER_CMD install"
+
+yes | $UNINSTALL_CMD surfraw-tools
+$INSTALL_CMD git+git://github.com/hoboneer/surfraw-elvis-generator.git#egg=surfraw_tools
