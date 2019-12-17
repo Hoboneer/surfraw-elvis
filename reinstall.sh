582d029ce1f28a74b6125aa12d048455825614b0
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2019-12-17 21:54:55 +1300

    Revert "Reinstall from 'list-enum-options' branch"
    This reverts commit b710b7f6213ebe29c062edbea6023b0057643342.

--- reinstall.sh
+++ reinstall.sh
@@ -5,4 +5,4 @@ UNINSTALL_CMD="$PKG_MANAGER_CMD uninstall"
 INSTALL_CMD="$PKG_MANAGER_CMD install"
 
 yes | $UNINSTALL_CMD surfraw-tools
-$INSTALL_CMD git+git://github.com/hoboneer/surfraw-elvis-generator.git@list-enum-options#egg=surfraw_tools
+$INSTALL_CMD git+git://github.com/hoboneer/surfraw-elvis-generator.git#egg=surfraw_tools
